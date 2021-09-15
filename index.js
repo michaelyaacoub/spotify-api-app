bdf6e9eb6759f05d0dc5162a6950f6e14a2afdf7
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-09-14 22:36:39 -0700

    oAuth

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
+const querystring = require('querystring');
 const app = express();
 const port = 8888;
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
+console.log(process.env.CLIENT_ID);
 //router handler
 app.get('/', (req, res) => {
   const data = {
@@ -13,10 +20,39 @@ app.get('/', (req, res) => {
   res.json(data);
 });
 
-app.get('/awesome-generator', (req, res) => {
-  const { name, isAwesome } = req.query;
-  res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
-});
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+ const generateRandomString = length => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+
+  const stateKey = 'spotify_auth_state';
+
+  app.get('/login', (req, res) => {
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    const scope = 'user-read-private user-read-email';
+
+    const queryParams = querystring.stringify({
+      client_id: CLIENT_ID,
+      response_type: 'code',
+      redirect_uri: REDIRECT_URI,
+      state: state,
+      scope: scope,
+    });
+
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+  });
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
