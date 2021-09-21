25d52327f399ed8f4d821b57335987a0ee9952ca
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-09-20 21:27:52 -0700

    adding dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,18 @@ function App() {
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const access_token = urlParams.get('access_token');
-    const refresh_token = urlParams.get('refresh_token');
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
 
-    console.log(access_token);
-    console.log(refresh_token);
+    console.log(accessToken);
+    console.log(refreshToken);
+
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+    }
   }, [])
   return (
     <div className="App">
