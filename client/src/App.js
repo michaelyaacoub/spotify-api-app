9a74ef73873991e5773677febdfa02843707d07a
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-09-21 21:26:14 -0700

    ADDED LOGIC TO localStorage to persist Login State

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
-import { useEffect, useState } from 'react'
-import { accessToken } from './ spotify';
+
+import { useState, useEffect } from 'react';
+import { accessToken, logout } from './spotify';
 import './App.css';
 
 function App() {
   const [token, setToken] = useState(null);
+
   useEffect(() => {
-    setToken(accessToken)
-  }, [])
+    setToken(accessToken);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         {!token ? (
-        <a className="App-link" href="http://localhost:8888/login">
-          Log in to Spotify
-        </a>
+          <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
+          </a>
         ) : (
-          <h1>Logged in!</h1>
+          <>
+            <h1>Logged in!</h1>
+            <button onClick={logout}>Log Out</button>
+          </>
         )}
       </header>
     </div>
