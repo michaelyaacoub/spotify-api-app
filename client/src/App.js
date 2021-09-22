65954c20806b3962a8f2d5cf3178664e7ef52364
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-09-21 21:11:14 -0700

    created access token and managed state to when logged in

--- client/src/App.js
+++ client/src/App.js
@@ -23,3 +23,18 @@ function App() {
 }
 
 export default App;
+
+
+
+// sessionStorage stores data for duration of the page session
+// localStorage keeps data stored even after the broswer is closed
+
+// -------------- gameplan -------------- //
+
+// 1- Upon first visit. log in, then store tokens from query params in localStorage
+// 2- Store timestamp in localStorage
+// 3- With next API call, first check for stored tokens
+// 4- check if the use stored timestamp to make sire tokens are not expired
+//     * if accessToken is valid, use that in our API request
+//     * if accessToken is expired, use refresh token to hit /refresh_token
+//     + When we reveice a new token, store it in localStorage and update timestamp
