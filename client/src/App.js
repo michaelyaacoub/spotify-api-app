7a16095ea8606e860c1a2d4d49f22792230f76b7
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-09-21 20:35:03 -0700

    created access token and managed state to when logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,22 @@
-import logo from './logo.svg';
-import {useEffect} from 'react'
+import { useEffect, useState } from 'react'
+import { accessToken } from './ spotify';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState(null);
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
-
-    console.log(accessToken);
-    console.log(refreshToken);
-
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err));
-    }
+    setToken(accessToken)
   }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {!token ? (
         <a className="App-link" href="http://localhost:8888/login">
           Log in to Spotify
         </a>
+        ) : (
+          <h1>Logged in!</h1>
+        )}
       </header>
     </div>
   );
