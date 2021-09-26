fa03759bbb32722484d7b4822b5c7318534025dd
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-09-25 23:41:05 -0700

    logout btn

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-import { BrowserRouter as Router, Switch, Route, useLocation } from "react-router-dom";
-import { useState, useEffect } from 'react';
-import { accessToken, logout, getCurrentUserProfile } from './spotify';
-import { catchErrors } from './utils';
-import styled from 'styled-components/macro';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { accessToken, logout, getCurrentUserProfile } from "./spotify";
+import { catchErrors } from "./utils";
+import styled from "styled-components/macro";
 import { GlobalStyle } from "./styles";
-import { Login , Profile} from "./pages";
+import { Login, Profile } from "./pages";
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0, 0, 0, 0.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
 
 // Scroll to top of page when changing routes
 function ScrollToTop() {
@@ -19,7 +40,7 @@ function ScrollToTop() {
 
 function App() {
   const [token, setToken] = useState(null);
-  const [profile, setProfile] = useState(null)
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
@@ -27,7 +48,7 @@ function App() {
     const fetchData = async () => {
       const { data } = await getCurrentUserProfile();
       setProfile(data);
-      console.log(data)
+      console.log(data);
     };
 
     catchErrors(fetchData());
@@ -41,29 +62,27 @@ function App() {
           <Login />
         ) : (
           <>
-
-          <button onClick={logout}>Log Out</button>
-
-          <Router>
-            <ScrollToTop />
-            <Switch>
-              <Route path="/top-artists">
-                <h1>Top Artists</h1>
-              </Route>
-              <Route path="/top-tracks">
-                <h1>Top Tracks</h1>
-              </Route>
-              <Route path="/playlists/:id">
-                <h1>Playlist</h1>
-              </Route>
-              <Route path="/playlists">
-                <h1>Playlists</h1>
-              </Route>
-              <Route path="/">
-                <Profile />
-              </Route>
-            </Switch>
-          </Router>
+            <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+            <Router>
+              <ScrollToTop />
+              <Switch>
+                <Route path="/top-artists">
+                  <h1>Top Artists</h1>
+                </Route>
+                <Route path="/top-tracks">
+                  <h1>Top Tracks</h1>
+                </Route>
+                <Route path="/playlists/:id">
+                  <h1>Playlist</h1>
+                </Route>
+                <Route path="/playlists">
+                  <h1>Playlists</h1>
+                </Route>
+                <Route path="/">
+                  <Profile />
+                </Route>
+              </Switch>
+            </Router>
           </>
         )}
       </header>
@@ -72,8 +91,6 @@ function App() {
 }
 
 export default App;
-
-
 
 // sessionStorage stores data for duration of the page session
 // localStorage keeps data stored even after the broswer is closed
