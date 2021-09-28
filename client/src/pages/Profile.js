50ccff4c0943ad5204c6052025553e6ed457a4c7
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-09-27 19:12:24 -0700

    profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,21 @@
 import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
-import { getCurrentUserProfile } from '../spotify';
+import { getCurrentUserProfile, getCurrentUserPlaylists } from '../spotify';
 import { StyledHeader } from '../styles';
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
+  const [playlists, setPlaylist] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await getCurrentUserProfile();
-      setProfile(data);
+      const userProfile = await getCurrentUserProfile();
+      setProfile(userProfile.data);
+      console.log(userProfile.data);
+
+      const userPlaylists = await getCurrentUserPlaylists();
+      setPlaylist(userPlaylists.data);
+      console.log(userPlaylists.data)
     };
 
     catchErrors(fetchData());
@@ -22,12 +28,15 @@ const Profile = () => {
           <StyledHeader type="user">
             <div className="header__inner">
               {profile.images.length && profile.images[0].url && (
-                <img className="header__img" src={profile.images[0].url} alt="Avatar"/>
+                <img className="header__img" src={profile.images[0].url} alt="Avatar" />
               )}
               <div>
                 <div className="header__overline">Profile</div>
                 <h1 className="header__name">{profile.display_name}</h1>
                 <p className="header__meta">
+                  {playlists && (
+                    <span>{playlists.total} Playlist{playlists.total !== 1 ? 's' : ''}</span>
+                  )}
                   <span>
                     {profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ''}
                   </span>
