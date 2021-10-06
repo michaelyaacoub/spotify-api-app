1ea816363215142068c5caaf5d9ac24badbf76e6
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-10-06 09:16:17 -0700

    added loader

--- client/src/Components/Loader.js
+++ client/src/Components/Loader.js
@@ -7,3 +7,26 @@ to {
     height: 100%;
 }
 `;
+
+const StyledLoader = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 100%;
+min-height: 50vh;
+
+.bars {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    overflow: hidden;
+    width: 100px;
+    min-height: 100px;
+    height: 50px;
+    margin: 0 auto;
+    z-index: 2;
+    position: relative;
+    left: 0;
+    right: 0;
+}
+`;
